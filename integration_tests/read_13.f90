52e31ac804b937d381759ea9574287e71e6609a5
Christopher Albert <albert@tugraz.at>
2026-01-01 22:08:02 +0100

    tests: add read_13 integration regression

---
+++ integration_tests/read_13.f90
@@ -0,0 +1,15 @@
+program read_13
+    use, intrinsic :: iso_fortran_env, only: int64
+    implicit none
+
+    integer(int64) :: x
+    integer :: unit, ios
+
+    open(newunit=unit, status="scratch", action="readwrite")
+    write(unit, "(A)") "abc"
+    rewind(unit)
+    read(unit, *, iostat=ios) x
+    close(unit)
+
+    if (ios <= 0) error stop
+end program read_13
