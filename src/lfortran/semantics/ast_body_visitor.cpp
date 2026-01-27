8438a292a1c02bef74937d9abab244d9e85a7e96
Christopher Albert <albert@tugraz.at>
2026-01-27 12:37:35 +0100

    fix: use correct integer types for ArrayUBound in ILP64 mode
    When using -fdefault-integer-8, legacy array sections used hardcoded
    Integer(4) constants for array bound operations, causing LLVM type
    mismatches like: "sub i64 %X, i32 1"
    
    Fixes:
    1. ast_common_visitor.h: Pass default_integer_kind to ArrayUBound calls
       and replace b.i32() with b.i_t() in legacy array section code
    2. asr_utils.h: Add index_kind parameter to get_bound template
    
    Test: legacy_array_sections_15.f90 - fails on main, passes with fix

--- src/lfortran/semantics/ast_body_visitor.cpp
+++ src/lfortran/semantics/ast_body_visitor.cpp
@@ -3180,7 +3180,7 @@ public:
         v->m_body = body.p;
         v->n_body = body.size();
 
-        replace_ArrayItem_in_SubroutineCall(al, compiler_options.legacy_array_sections, current_scope);
+        replace_ArrayItem_in_SubroutineCall(al, compiler_options.legacy_array_sections, current_scope, compiler_options.po.default_integer_kind);
 
         for (size_t i=0; i<x.n_contains; i++) {
             visit_program_unit(*x.m_contains[i]);
@@ -3674,7 +3674,7 @@ public:
         v->m_dependencies = func_deps.p;
         v->n_dependencies = func_deps.size();
 
-        replace_ArrayItem_in_SubroutineCall(al, compiler_options.legacy_array_sections, current_scope);
+        replace_ArrayItem_in_SubroutineCall(al, compiler_options.legacy_array_sections, current_scope, compiler_options.po.default_integer_kind);
 
         for (size_t i=0; i<x.n_contains; i++) {
             visit_program_unit(*x.m_contains[i]);
@@ -3748,7 +3748,7 @@ public:
         v->m_dependencies = func_deps.p;
         v->n_dependencies = func_deps.size();
 
-        replace_ArrayItem_in_SubroutineCall(al, compiler_options.legacy_array_sections, current_scope);
+        replace_ArrayItem_in_SubroutineCall(al, compiler_options.legacy_array_sections, current_scope, compiler_options.po.default_integer_kind);
 
         for (size_t i=0; i<x.n_contains; i++) {
             visit_program_unit(*x.m_contains[i]);
